0984742a20658ebedb0f6287e5b576341813af21
Juan Aristizabal <juanaristi2001@gmail.com>
2024-06-05 19:00:56 -0500

    Add User Authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
+import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./models/contexts/AuthContext";
+import Register from "./views/Register";
+import Login from "./views/Login";
+import List from "./views/supplier/List";
+import New from "./views/supplier/New";
+import Selection from "./views/mechanic/Selection";
+
+const RequireAuth = ({ children }) => {
+  const { currentUser } = useContext(AuthContext);
+  return currentUser ? children : <Navigate to="/login" />;
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path=":supplierId">
+              <Route
+                path="list"
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                    <New />
+                  </RequireAuth>
+                }
+              />
+            </Route>
+            <Route path=":mechanicId">
+              <Route
+                path="catalog"
+                element={
+                  <RequireAuth>
+                    <Selection />
+                  </RequireAuth>
+                }
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
